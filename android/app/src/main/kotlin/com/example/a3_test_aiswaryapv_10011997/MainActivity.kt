2e415e3a3b0534b4bfbff2be719d043fd3082849
Aiswarya  P V <aiswaryapv3123@gmail.com>
2021-12-08 20:28:14 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/a3_test_aiswaryapv_10011997/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a3_test_aiswaryapv_10011997
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:a3_test_aiswaryapv_10011997/src/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: HomePage()
+    );
+  }
+}
+
